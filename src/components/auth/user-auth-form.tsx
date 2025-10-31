1f1e301e7b141aa4ec028e5d75051b25a1dbfc21
Srinivasan .U <usrinivasan240@gmail.com>
2025-10-31 03:16:27 +0000

    remove this google and phone opentional and add email authandication but

--- src/components/auth/user-auth-form.tsx
+++ src/components/auth/user-auth-form.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Github, Phone } from 'lucide-react';
+import { Mail } from 'lucide-react';
 import Link from 'next/link';
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -80,31 +80,11 @@ export function UserAuthForm({ className, role, ...props }: UserAuthFormProps) {
                   ></path>
                 </svg>
               )}
-              Sign In as {role.charAt(0).toUpperCase() + role.slice(1)}
+              Sign In
             </Button>
           </Link>
         </div>
       </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <div className="grid grid-cols-2 gap-4">
-        <Button variant="outline" type="button" disabled={isLoading}>
-          <Github className="mr-2 h-4 w-4" />
-          Google
-        </Button>
-        <Button variant="outline" type="button" disabled={isLoading}>
-          <Phone className="mr-2 h-4 w-4" />
-          Phone
-        </Button>
-      </div>
     </div>
   );
 }
