2d57f42f3816a0c37f0ce41a39eed069cacca486
Srinivasan .U <usrinivasan240@gmail.com>
2025-11-03 12:09:04 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-3961561366-c31f7",
-  "appId": "1:843805677803:web:a8a4e5d8713db6e40386ae",
-  "apiKey": "AIzaSyDAEwc_xcR4qish1ekhxZBOsasdiy9jl0w",
-  "authDomain": "studio-3961561366-c31f7.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "843805677803"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
