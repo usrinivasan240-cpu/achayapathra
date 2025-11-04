6ee7c681a40fd52f74603b331df1e0eea00222b5
Srinivasan .U <usrinivasan240@gmail.com>
2025-11-04 15:19:14 +0000

    change all symboles into ruppess

--- src/components/dashboard/overview.tsx
+++ src/components/dashboard/overview.tsx
@@ -33,7 +33,7 @@ export function Overview() {
           fontSize={12}
           tickLine={false}
           axisLine={false}
-          tickFormatter={(value) => `$${value}`}
+          tickFormatter={(value) => `₹${value}`}
         />
         <Bar dataKey="total" fill="var(--chart-1)" radius={[4, 4, 0, 0]} />
       </BarChart>
