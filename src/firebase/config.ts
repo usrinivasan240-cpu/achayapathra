28b1777f82a494851b164eb3cbc6d0ab040affea
Srinivasan .U <usrinivasan240@gmail.com>
2025-11-05 15:23:53 +0000

    create a storeage in data base save this  and submit this donation page

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "studio-3961561366-c31f7",
   "appId": "1:843805677803:web:a8a4e5d8713db6e40386ae",
   "apiKey": "AIzaSyDAEwc_xcR4qish1ekhxZBOsasdiy9jl0w",
   "authDomain": "studio-3961561366-c31f7.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "843805677803"
+  "messagingSenderId": "843805677803",
+  "storageBucket": "studio-3961561366-c31f7.appspot.com"
 };
